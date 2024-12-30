40865406320abfc32a556be53009d5280966b6d7
vinhraudaklak <vinhpro0972@gmail.com>
2024-12-30 22:09:53 +0700

    Add header

---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+import logo from './Logo.png';
+
+export const images = {
+  logo,
+}
--- src/components/Layout/Header/Header.jsx
+++
@@ -1,7 +0,0 @@
-const Header = () => {
-  return (
-    <div>Header sdsd</div>
-  )
-}
-
-export default Header
